0c5e5dd9eb36030762eabd0d9f2be264b87d612f
Yorgos Saslis <yorgos.work@proton.me>
2024-10-14 17:11:20 +0300

    keeps just httpd
    Signed-off-by: Yorgos Saslis <yorgos.work@proton.me>

--- tests/visual/desktop/notFound.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "@tests/support/fixtures.js";
-
-test("page not found", async ({ page }) => {
-  await page.goto("/this/page/does/not/exist", {
-    waitUntil: "networkidle",
-  });
-  await expect(page).toHaveScreenshot();
-});
