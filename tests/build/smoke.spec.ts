4b4c12103c6be4b9be537779a992af05ad6591ed
Yorgos Saslis <yorgos.work@proton.me>
2024-10-14 17:11:20 +0300

    radicle-httpd becomes radicle-http-api
    updates readme
    renames to avoid confusion
    adds missing licenses from upstream (no license change)
    adds concourse pipelines to build container image (we will also move GH actions jobs here)
    
    Signed-off-by: Yorgos Saslis <yorgos.work@proton.me>

--- tests/build/smoke.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@tests/support/fixtures.js";
-
-test("exceptions in production build", async ({ page }) => {
-  await page.goto("/");
-  // Wait for scripts to finish executing, there might be exceptions that
-  // happen after the page has been painted.
-  await page.waitForTimeout(2000);
-  await expect(page.getByText("Explore", { exact: true })).toBeVisible();
-});
